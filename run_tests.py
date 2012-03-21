d0afd7b3ac68007de999195f7df1235b0c7a34ff
Kyle Wilcox <kwilcox@asascience.com>
2012-03-21 14:18:03 -0400

    Only load tests from the test folder

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,7 @@ import unittest
 
 # First, discover all tests in the project
 loader = unittest.TestLoader()
-tests = loader.discover('.')
+tests = loader.discover('./src/test')
 
 # Create a runner and run those tests.
 testRunner = unittest.runner.TextTestRunner()
