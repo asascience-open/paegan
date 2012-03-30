776e6e91e7b9ccb86f7421c36edfeab5c8d2ac2a
Erick Geiger <egeiger@asascience.com>
2012-03-30 12:09:46 -0400

    added set and get dispersion coefficients to transport model

--- src/transport/models/transport.py
+++ src/transport/models/transport.py
@@ -5,10 +5,32 @@ from src.utils.asarandom import AsaRandom
 
 class Transport:
     """
-        Transport a particle in the x y and z
+        Transport a particle in the x y and z direction. Requires horizontal and vertical dispersion coefficients.
+        Will only move particle when self.move() is called with the proper arguments.
     """
+
+    def __init__(self, **kwargs):
+
+        if "horizDisp" and "vertDisp" in kwargs:
+            self._horizDisp = kwargs.pop('horizDisp')
+            self._vertDisp = kwargs.pop('vertDisp')
+        else:
+            raise TypeError( "must provide a horizontal and vertical dispersion coefficient (horizDisp and vertDisp)" )
+
+    def set_horizDisp(self, hdisp):
+        self._horizDisp = hdisp
+    def get_horizDisp(self):
+        return self._horizDisp
+    horizDisp = property(get_horizDisp, set_horizDisp)
+
+    def set_vertDisp(self, vdisp):
+        self._vertDisp = vdisp
+    def get_vertDisp(self):
+        return self._vertDisp
+    vertDisp = property(get_vertDisp, set_vertDisp)
+
     
-    def move(self, lat, lon, depth, u, v, z, horizDisp, vertDisp, modelTimestep):
+    def move(self, lat, lon, depth, u, v, z, modelTimestep):
         """
         Returns the lat, lon, H, and velocity of a projected point given a starting
         lat and lon (dec deg), a depth (m) below sea surface, u, v, and z velocity components (m/s), a horizontal and vertical
@@ -24,9 +46,9 @@ class Transport:
         f = (rmajor - rminor) / rmajor # calculating flattening (vincenty)
         #f = 1 / 298.257223563 # WGS-84 ellipsoid flattening parameter (vincenty)
 
-        u += AsaRandom.random() * ((2 * horizDisp / modelTimestep) ** 0.5) # u transformation calcualtions
-        v += AsaRandom.random() * ((2 * horizDisp / modelTimestep) ** 0.5) # v transformation calcualtions
-        z += AsaRandom.random() * ((2 * vertDisp / modelTimestep) ** 0.5) # z transformation calculations
+        u += AsaRandom.random() * ((2 * self.horizDisp / modelTimestep) ** 0.5) # u transformation calcualtions
+        v += AsaRandom.random() * ((2 * self.horizDisp / modelTimestep) ** 0.5) # v transformation calcualtions
+        z += AsaRandom.random() * ((2 * self.vertDisp / modelTimestep) ** 0.5) # z transformation calculations
 
         # Move horizontally
         s_and_d = AsaMath.speed_direction_from_u_v(u=u,v=v) # calculates velocity in m/s from transformed u and v
@@ -46,3 +68,9 @@ class Transport:
         lon_result = math.degrees(lon_result)
 
         return {'lat':lat_result, 'lon':lon_result, 'reverse_angle':angle_result, 'depth':depth, 'u': u, 'v':v, 'z':z}
+
+    def __str__(self):
+        return  " *** Transport *** " + \
+                "\nhorizDisp: " + str(self.horizDisp) + \
+                "\nvertDisp: " + str(self.vertDisp)
+
