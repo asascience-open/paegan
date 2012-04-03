fe5211b2d5e2f2c4d8eef8be73c3f5457ac8f096
Kyle Wilcox <kwilcox@asascience.com>
2012-04-03 15:13:08 -0400

    Drop in the water

--- src/test/test_model_controller.py
+++ src/test/test_model_controller.py
@@ -14,15 +14,14 @@ from src.transport.model_controller import ModelController
 class ModelControllerTest(unittest.TestCase):
     def test_particles(self):
         # Set the start position and time for the models
-        start_lat = 38
-        start_lon = -76
-        start_depth = -5
+        start_lat = 37.8
+        start_lon = -76.05
+        start_depth = 0
         num_particles = 3
         time_step = 3600
         num_steps = 50
         temp_time = datetime.utcnow()
+        models = [Transport]
         start_time = datetime(temp_time.year, temp_time.month, temp_time.day, temp_time.hour)
-        model = ModelController(latitude=start_lat, longitude=start_lon, depth=start_depth, start=start_time, step=time_step, nstep=num_steps, npart=num_particles, models=[Transport], use_bathymetry=False, use_shoreline=True)
-        # Take in a dataset
+        model = ModelController(latitude=start_lat, longitude=start_lon, depth=start_depth, start=start_time, step=time_step, nstep=num_steps, npart=num_particles, models=models, use_bathymetry=False, use_shoreline=True)
         model.run()
-        print model
