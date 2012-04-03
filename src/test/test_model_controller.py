2f7a3e9aee351c46a72ba723c4f16f76c42b1ce3
Erick Geiger <egeiger@asascience.com>
2012-04-03 09:56:54 -0400

    changes

--- src/test/test_model_controller.py
+++ src/test/test_model_controller.py
@@ -17,10 +17,12 @@ class ModelControllerTest(unittest.TestCase):
         start_lat = 38
         start_lon = -76
         start_depth = -5
+        num_particles = 3
+        time_step = 3600
+        num_steps = 50
         temp_time = datetime.utcnow()
         start_time = datetime(temp_time.year, temp_time.month, temp_time.day, temp_time.hour)
-        model = ModelController(latitude=start_lat, longitude=start_lon, depth=start_depth, start=start_time, step=3600, nstep=10, npart=1, models=[Transport], use_bathymetry=False, use_shoreline=False)
+        model = ModelController(latitude=start_lat, longitude=start_lon, depth=start_depth, start=start_time, step=time_step, nstep=num_steps, npart=num_particles, models=[Transport], use_bathymetry=False, use_shoreline=True)
         # Take in a dataset
         model.run()
         print model
-        
