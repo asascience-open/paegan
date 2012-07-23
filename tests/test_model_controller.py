160b4d0948b56ed838ddc00a34a81ec26f2669db
Alexander Crosby <acrosby@asascience.com>
2012-07-23 14:44:59 -0400

    updated model_controller test

--- tests/test_model_controller.py
+++ tests/test_model_controller.py
@@ -14,15 +14,17 @@ from paegan.transport.model_controller import ModelController
 class ModelControllerTest(unittest.TestCase):
     def test_run_individual_particles(self):
         # Set the start position and time for the models
-        start_lat = 39
-        start_lon = -75
-        start_depth = -5
+        start_lat = 60.75
+        start_lon = -147
+        start_depth = 0
         num_particles = 10
         time_step = 3600
-        num_steps = 10
+        num_steps = 2
         temp_time = datetime.utcnow()
         models = [Transport(horizDisp=0.05, vertDisp=0.0003)]
         start_time = datetime(temp_time.year, temp_time.month, temp_time.day, temp_time.hour)
-        model = ModelController(latitude=start_lat, longitude=start_lon, depth=start_depth, start=start_time, step=time_step, nstep=num_steps, npart=num_particles, models=models, use_bathymetry=False, use_shoreline=True)
-        model.run()
-        model.generate_map()
+        model = ModelController(latitude=start_lat, longitude=start_lon, depth=start_depth, start=start_time, step=time_step, nstep=num_steps, npart=num_particles, models=models, use_bathymetry=False, use_shoreline=True,
+            time_chunk=1)
+        model.run("http://thredds.axiomalaska.com/thredds/dodsC/PWS_L2_FCST.nc")
+        fig = model.generate_map(Point(start_lon, start_lat))
+        fig.savefig('test_model_controller.png')
