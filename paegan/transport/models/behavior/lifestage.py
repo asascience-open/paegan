36a8f62e6e3c01b099727fe3bbf8516eddf517dc
Kyle Wilcox <kwilcox@asascience.com>
2012-08-09 11:12:37 -0400

    Add LifeStage

--- paegan/transport/models/behavior/lifestage.py
+++ paegan/transport/models/behavior/lifestage.py
@@ -1,7 +1,29 @@
+import json
+from paegan.transport.models.behavior.diel import Diel
+from paegan.transport.models.behavior.taxis import Taxis
+from paegan.transport.models.behavior.capability import Capability
+
 class LifeStage(object):
 
-	def __init__(self):
-		pass
+    def __init__(self, **kwargs):
 
-	def get_models(self):
-		pass
+        if 'json' in kwargs or 'data' in kwargs:
+            data = {}
+            try:
+                data = json.loads(kwargs['json'])
+            except:
+                try:
+                    data = kwargs.get('data')
+                except:
+                    pass
+
+            self.name = data.get('name',None)
+            self.linear_a = data.get('linear_a', None)
+            self.linear_b = data.get('linear_b', None)
+            self.duration = data.get('duration', None)
+            self.diel = [Diel(data=d) for d in data.get('diel')]
+            self.taxis = [Taxis(data=t) for t in data.get('taxis')]
+            self.capability = Capability(data=data.get('capability'))
+
+    def get_models(self):
+        print len(self.diel)
---
+++ tests/test_lifestage.py
@@ -0,0 +1,55 @@
+import unittest
+from paegan.transport.models.behavior.lifestage import LifeStage
+import os
+import json
+from datetime import datetime
+import pytz
+from paegan.transport.location4d import Location4D
+
+class LifeStageTest(unittest.TestCase):
+
+    def test_from_json(self):
+        data = open(os.path.normpath(os.path.join(os.path.dirname(__file__),"./resources/files/lifestage_single.json"))).read()
+        l = LifeStage(json=data)
+
+        assert l.name == 'third'
+        assert l.duration == 3
+        assert l.linear_a == None
+        assert l.linear_b == None
+        assert len(l.taxis) == 2
+        assert l.taxis[0].min_value == 30.0
+        assert l.taxis[0].max_value == 40.0        
+        assert len(l.diel) == 2
+        assert l.diel[0].min_depth == 2.0
+        assert l.diel[0].max_depth == 4.0
+        assert l.capability.vss == 5.0
+        assert l.capability.variance == 2.0
+        assert l.capability.non_swim_turning == 'random'
+        assert l.capability.swim_turning == 'random'
+
+        t = datetime.utcnow().replace(tzinfo=pytz.utc)
+        loc = Location4D(time=t, latitude=35, longitude=-76)
+        assert isinstance(l.diel[0].get_time(loc4d=loc), datetime)
+
+    def test_from_dict(self):
+        data = open(os.path.normpath(os.path.join(os.path.dirname(__file__),"./resources/files/lifestage_single.json"))).read()
+        l = LifeStage(data=json.loads(data))
+
+        assert l.name == 'third'
+        assert l.duration == 3
+        assert l.linear_a == None
+        assert l.linear_b == None
+        assert len(l.taxis) == 2
+        assert l.taxis[1].min_value == 30.0
+        assert l.taxis[1].max_value == 50.0        
+        assert len(l.diel) == 2
+        assert l.diel[1].min_depth == 2.0
+        assert l.diel[1].max_depth == 5.0
+        assert l.capability.vss == 5.0
+        assert l.capability.variance == 2.0
+        assert l.capability.non_swim_turning == 'random'
+        assert l.capability.swim_turning == 'random'
+
+        t = datetime.utcnow().replace(tzinfo=pytz.utc)
+        loc = Location4D(time=t, latitude=35, longitude=-76)
+        assert isinstance(l.diel[0].get_time(loc4d=loc), datetime)
