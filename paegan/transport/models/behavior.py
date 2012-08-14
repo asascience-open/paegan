80e4100dde1c0ac093858039db3ac3ce6b131aef
Kyle Wilcox <kwilcox@asascience.com>
2012-08-14 14:15:37 -0400

    Actually return the results of the behavior model

--- paegan/transport/models/behavior.py
+++ paegan/transport/models/behavior.py
@@ -16,9 +16,10 @@ class LarvaBehavior(BaseModel):
                 except:
                     pass
 
+        self.lifestages = []
         if data.get('lifestages', None) is not None:
             self.lifestages = [LifeStage(data=ls) for ls in data.get('lifestages')]
 
     def move(self, particle, u, v, z, modelTimestep, **kwargs):
         # Only run that lifestage model that corresponds to the particles growth progress
-        self.lifestages[particle.lifestage_index].move(particle, u, v, z, modelTimestep, **kwargs)
+        return self.lifestages[particle.lifestage_index].move(particle, u, v, z, modelTimestep, **kwargs)
