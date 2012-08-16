97979b2aa860570f8faa827a929af0eb83302ba2
Kyle Wilcox <kwilcox@asascience.com>
2012-08-16 11:43:39 -0400

    Add PyPI reqs.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages, Command
+
+files = ["paegan/*"]
+readme = open('README.txt', 'rb').read()
+reqs = [line.strip() for line in open('requirements.txt')]
+
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+setup(name              = "paegan",
+    version             = "0.8.0",
+    description         = "Processing and Analysis for Numerical Data",
+    long_description    = readme,
+    license             = 'LICENSE.txt',
+    author              = "Kyle Wilcox",
+    author_email        = "kwilcox@sasascience.com",
+    url                 = "https://github.com/asascience-open/paegan",
+    packages            = find_packages(),
+    cmdclass            = {'test': PyTest},
+    install_requires    = reqs,
+    classifiers         = [
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering',
+        ],
+) 
