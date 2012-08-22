af186050b9bbe1da411eca218b4b44737535a79e
Kyle Wilcox <kwilcox@asascience.com>
2012-08-22 11:40:19 -0400

    Prepare for 0.8.7 release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class PyTest(Command):
         raise SystemExit(errno)
 
 setup(name              = "paegan",
-    version             = "0.8.6",
+    version             = "0.8.7",
     description         = "Processing and Analysis for Numerical Data",
     long_description    = readme,
     license             = 'LICENSE.txt',
