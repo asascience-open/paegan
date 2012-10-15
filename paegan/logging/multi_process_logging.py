76c7fb924a996c37d2ace09035f5a8c9dd5fbfa1
Kyle Wilcox <kwilcox@asascience.com>
2012-10-15 15:44:34 -0400

    Clean up the logger to use StopIteration to close the queue

--- paegan/logging/multi_process_logging.py
+++ paegan/logging/multi_process_logging.py
@@ -17,29 +17,21 @@ class MultiProcessingLogHandler(logging.Handler):
         self._handler.setFormatter(fmt)
 
     def receive(self):
-        while self.queue.get():
+        while True:
             try:
+                record = self.queue.get()
+                if record == StopIteration:
+                    break
                 self._handler.emit(record)
             except (KeyboardInterrupt, SystemExit):
                 raise
+            except EOFError:
+                break
             except:
                 traceback.print_exc(file=sys.stderr)
                 break
 
-
-        # while True:
-        #     try:
-        #         record = self.queue.get()
-        #         self._handler.emit(record)
-        #     except (KeyboardInterrupt, SystemExit):
-        #         raise
-        #     except (EOFError, MemoryError):
-        #         break
-        #     except:
-        #         traceback.print_exc(file=sys.stderr)
-        #         break
-
-        # return
+        return
 
     def send(self, s):
         self.queue.put_nowait(s)
@@ -68,29 +60,44 @@ class MultiProcessingLogHandler(logging.Handler):
             self.handleError(record)
 
     def close(self):
+        self.queue.put_nowait(StopIteration)
         self._handler.close()
         logging.Handler.close(self)
         
 class EasyLogger(object):
-    def __init__(self, logpath):
+    def __init__(self, logpath, level=None):
+
+        if level is None:
+            level = logging.INFO
+
         self.logpath = logpath
         self.logger = multiprocessing.get_logger()
 
+        self.queue = multiprocessing.Queue(-1)
+
         # Close any existing handlers
-        self.close()
+        self.close_handlers()
+        
         # Remove any existing handlers
         self.logger.handlers = []
+        self.logger.setLevel(level)
 
-        self.logger.setLevel(logging.INFO)
-        handler = MultiProcessingLogHandler(logpath)
-        handler.setLevel(logging.DEBUG)
+        handler = MultiProcessingLogHandler(logpath, self.queue)
+        handler.setLevel(level)
         formatter = logging.Formatter('[%(asctime)s] - %(levelname)s - %(name)s - %(processName)s - %(message)s')
         handler.setFormatter(formatter)
         # Add handler
         self.logger.addHandler(handler)
         
-    def close(self):
+    def close_handlers(self):
         (hand.close() for hand in self.logger.handlers)
+        
+    def close_queue(self):
+        self.queue.put_nowait(StopIteration)
+
+    def close(self):
+        self.close_handlers()
+        self.close_queue()
 
     def __str__(self):
         return "Logging with MultiProcessingLogHandler in " + self.logpath
