58416ff34d1c3eba9e25cb2e18774d96dba3c440
Kyle Wilcox <kwilcox@asascience.com>
2013-01-11 14:18:04 -0500

    simpler calculation since particle_time is always >=

--- paegan/transport/models/behaviors/lifestage.py
+++ paegan/transport/models/behaviors/lifestage.py
@@ -86,7 +86,8 @@ class LifeStage(BaseModel):
             for ad in self.diel:
                 d_time = ad.get_time(loc4d=particle.location)
                 if d_time <= particle_time:
-                    seconds = abs((d_time - particle_time).total_seconds())
+                    seconds = (particle_time - d_time).total_seconds()
+                    #logger.info("%s : %s : %s" % (str(d_time), ad.logstring(), seconds))
                     if closest is None or seconds < closest_seconds:
                         closest = ad
                         closest_seconds = seconds
