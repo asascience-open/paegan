ea221327f0c43e8079f5e86e298f269a467e7206
Kyle Wilcox <kwilcox@asascience.com>
2013-02-20 13:29:29 -0500

    Bathy should ignore time and leave it up to the calling function.

--- paegan/transport/bathymetry.py
+++ paegan/transport/bathymetry.py
@@ -2,6 +2,7 @@ import os
 import netCDF4
 from paegan.cdm.dataset import CommonDataset
 from paegan.transport.location4d import Location4D
+
 import numpy as np
 
 class Bathymetry():
@@ -42,7 +43,10 @@ class Bathymetry():
         return np.mean(np.mean(self._nc.get_values(self._bathy_name, point=location)))
 
     def react(self, **kwargs):
-    
+        """
+            The time of recation is ignored hereTime is ignored here 
+            and should be handled by whatever called this function.
+        """
         react_type = kwargs.get("type", self._type)
 
         if react_type == 'hover':
@@ -57,17 +61,18 @@ class Bathymetry():
     def __hover(self, **kwargs):
         """
             This hovers the particle 1m above the bathymetry WHERE IT WOULD HAVE ENDED UP.
-            This is most likely wrong and we need to compute the location that it actually hit
-            the bathymetry and hover 1m above THAT point.
+            This is WRONG and we need to compute the location that it actually hit
+            the bathymetry and hover 1m above THAT.
         """
         end_point = kwargs.pop('end_point')
+        # The location argument here should be the point that intersected the bathymetry,
+        # not the end_point that is "through" the bathymetry.
         depth = self.get_depth(location=end_point)
-        return Location4D(time=end_point.time, latitude=end_point.latitude, longitude=end_point.longitude, depth=(depth + 1.))
+        return Location4D(latitude=end_point.latitude, longitude=end_point.longitude, depth=(depth + 1.))
         
     def __reverse(self, **kwargs):
         """
-            If we hit the bathymetry, set the location to where we came from, but increase the time.
+            If we hit the bathymetry, set the location to where we came from.
         """
         start_point = kwargs.pop('start_point')
-        end_point = kwargs.pop('end_point')
-        return Location4D(time=end_point.time, latitude=start_point.latitude, longitude=start_point.longitude, depth=start_point.depth)
+        return Location4D(latitude=start_point.latitude, longitude=start_point.longitude, depth=start_point.depth)
